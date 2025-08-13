ec52a8a17ec536512aba690d0f277452e3482fa5
JoaoLucas <lucasrpg.silva@gmail.com>
2025-08-12 20:05:07 -0700

    feat: title created

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <div className={styles.app}>
       <Header />
-      <h1>Hello World</h1>
+      <div className={styles.home}>
+        <div className={styles.top_page}>
+          <div className={styles.title}>
+            <h1 className={styles.title_text}>Hi,<br />
+                My name is <br />
+              <span className={styles.name}>João Lucas</span><br />
+              I build the future
+            </h1>
+          </div>
+          <img src="./src/assets/images/jl_image.JPG" alt="Joao Lucas" />
+        </div>
+      </div>
     </div>
   );
 }
