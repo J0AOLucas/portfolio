45023a6cfd95722cca94b806cbe63fa074d6a489
JoaoLucas <lucasrpg.silva@gmail.com>
2025-08-12 21:06:20 -0700

    feat: stacks added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './App.module.css';
 import Header from './components/Header/Header';
 
@@ -16,6 +15,29 @@ function App() {
             </h1>
           </div>
           <img src="./src/assets/images/jl_image.JPG" alt="Joao Lucas" />
+        </div>
+        <div className={styles.tech_stack}>
+          <h1>My Tech Stack</h1>
+          <h2>Technologies I've been working with recently</h2>
+          <div className={styles.tech_logos}>
+            <img src="./src/assets/icons/html-logo.svg" alt="html logo" />
+            <img src="./src/assets/icons/css-logo.svg" alt="css logo" />
+            <img src="./src/assets/icons/js-logo.svg" alt="js logo" />
+            <img src="./src/assets/icons/react-logo.svg" alt="react logo" />
+            <img src="./src/assets/icons/ruby-logo.svg" alt="ruby logo" />
+            <img src="./src/assets/icons/python-logo.svg" alt="python logo" />
+            <div>
+              <img src="./src/assets/icons/tailwind-logo.svg" alt="tailwind logo" />
+              <img src="./src/assets/icons/git-logo.svg" alt="git logo" />
+              <img src="./src/assets/icons/spring-logo.svg" alt="spring logo" />
+              <img src="./src/assets/icons/postgres-logo.svg" alt="postgres logo" />
+              <img src="./src/assets/icons/github-logo.svg" alt="github logo" />
+            </div>
+          </div>
+          <div className={styles.projects}>
+            <h1>Projects</h1>
+            <h2>Things I've built so far</h2>
+          </div>
         </div>
       </div>
     </div>
